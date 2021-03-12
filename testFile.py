32c93e5316687bf1f91e67ea98e3b0ad0828fb4b
Briac Ferte <briac.ferte@gmail.com>
2021-03-12 17:28:34 +0100

    first commit

--- testFile.py
+++ testFile.py
@@ -14,8 +14,7 @@ filesAbsolute = []
 for file in glob.glob("dossierRempli/*"):
     files.append(file)
 
-print(files)
-print('ici')
+
 for file in files:
     filesAbsolute.append(Path(file).resolve())
 
@@ -23,8 +22,7 @@ for file in filesAbsolute:
     file = str(file) 
     print(file)   
     
-print('ici')
-print(filesAbsolute)
+
 
 
 
@@ -37,15 +35,14 @@ import win32com.client as win32
 outlook = win32.Dispatch('Outlook.Application')
 mail = outlook.CreateItem(0)
 mail.Display()
-mail.To = 'To address'
-mail.Subject = 'Message subject'
-mail.Body = 'Message body'
-mail.HTMLBody = '<h2>HTML Message body</h2>' #this field is optional
+#mail.To = 'To address'
+#mail.Subject = 'Message subject'
+#mail.Body = 'Message body'
+#mail.HTMLBody = '<h2>HTML Message body</h2>' #this field is optional
 
 # To attach a file to the email (optional):
-attachment  = "Path to the attachment"
+#attachment  = "Path to the attachment"
 #mail.Attachments.Add("dossierRempli\pli.txt")
 for file in filesAbsolute:
-    print(file)
     mail.Attachments.Add(str(file))
 #mail.Send()
